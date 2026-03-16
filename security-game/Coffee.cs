c0dde4869dad4c874db7c2b53f90663f58793db4
Bumblebee10 <123257052+Bumblebee10@users.noreply.github.com>
2026-03-16 14:59:13 +0100

    Coffee added

---
+++ security-game/Coffee.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class Coffee : Node3D, IInteractible
+{
+	[Signal]
+	public delegate void DrinkEventHandler(Node3D interactor, Vector3 hitPosition);
+
+	public void Interact(Node3D interactor, Vector3 hitPosition)
+	{
+		GD.Print("Drink signal emitted");
+		EmitSignal(SignalName.Drink, 100);
+	}
+}
