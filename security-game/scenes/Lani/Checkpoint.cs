ec73e03ba87d8045b8ef1c7966f6459b1d109639
Bumblebee10 <123257052+Bumblebee10@users.noreply.github.com>
2026-03-16 14:29:08 +0100

    Reportable checkpoints
    - mutiple checkpoints
    - Can report to fix
    - Flashlight to player

--- security-game/scenes/Lani/Checkpoint.cs
+++ security-game/scenes/Lani/Checkpoint.cs
@@ -5,9 +5,30 @@ using System.ComponentModel;
 public partial class Checkpoint : StaticBody3D
 {
 	[Export] private Node3D _player;
+	[Export] private Timer anomalyTimer;
+	[Export] private Timer tempTimer;
+	[Export] public bool hasAnomaly;
+	[Export] public int ID = 1;
+	[Export] private AnimationPlayer animations;
 	
 	[Signal]
 	public delegate void LoseEnergyEventHandler();
+
+    public override void _Ready()
+    {
+		SetRandomWaitTime();
+        anomalyTimer.Start();
+		animations.Play("close");
+		anomalyTimer.Timeout += MakeAnomaly;
+		//tempTimer.Timeout += fixAnomaly;
+    }
+
+    public override void _Process(double delta)
+    {
+    }
+
+
+	//Roep signaal aan om energie te verliezen wanneer de speler binnen de range komt
 	private void OnPLayerEnteredCheckpoint(Node3D body)
 	{
 		if (body == _player)
@@ -16,4 +37,35 @@ public partial class Checkpoint : StaticBody3D
 			EmitSignal(SignalName.LoseEnergy);
 		}
 	}
+
+	// Maak een nieuwe anomaly (momenteel kan de deur alleen open gaan)
+	private void MakeAnomaly()
+	{
+		if (hasAnomaly)
+            return;
+		
+		animations.Play("open");
+		hasAnomaly = true;
+		//tempTimer.Start();
+	}
+
+	//Stel een nieuwe random tijd in om een nieuwe anomaly aan te maken
+	private void SetRandomWaitTime()
+	{
+		anomalyTimer.WaitTime = GD.RandRange(10, 60);
+	}
+
+	public void FixAnomaly()
+	{
+		if (!hasAnomaly)
+			return;
+
+		animations.Play("close");
+		hasAnomaly = false;
+		GD.Print($"Checkpoint {ID} anomaly fixed");
+
+        SetRandomWaitTime();
+        anomalyTimer.Start();
+	}
+
 }
