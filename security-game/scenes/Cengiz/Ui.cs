08bfe09a6f7039d66fc8af2a54b0e1787de8ec76
Bumblebee10 <123257052+Bumblebee10@users.noreply.github.com>
2026-03-17 08:43:53 +0100

    Test scene fix
    Mijn test scene gefixed en error code in Cengiz\UI.cs in commentaar gezet

--- security-game/scenes/Cengiz/Ui.cs
+++ security-game/scenes/Cengiz/Ui.cs
@@ -4,16 +4,16 @@ using System;
 public partial class Ui : Control
 {
 	ProgressBar energyBar;
-	Player player;
+	//Player player;
 
 	public override void _Ready()
 	{
 		energyBar = GetNode<ProgressBar>("EnergyBar");
-		player = GetNode<Player>("../Player"); // pas pad aan indien nodig
+		//player = GetNode<Player>("../Player"); // pas pad aan indien nodig
 	}
 
 	public override void _Process(double delta)
 	{
-		energyBar.Value = player.Energy;
+		//energyBar.Value = player.Energy;
 	}
 }
