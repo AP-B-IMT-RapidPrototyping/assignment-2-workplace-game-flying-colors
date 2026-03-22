81bc93e6662db5e33ea7de925947f78112cb0491
Bumblebee10 <123257052+Bumblebee10@users.noreply.github.com>
2026-03-22 15:03:46 +0100

    Tutorial finished
    - finished the tutorial level
    - Made sure mouse is visible on the title screen

---
+++ security-game/scenes/Lani/TutorialStuff/AreaEnd.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class AreaEnd : Area3D
+{
+	[Export] private CharacterBody3D _player;
+	[Export] private Label3D label;
+	[Export] private Timer endTimer;
+
+    public override void _Ready()
+    {
+        endTimer.Timeout += endTutorial;
+    }
+
+	
+	private void playerEntered(Node3D node)
+	{
+		if (node == _player)
+		{
+			GD.Print("player entered");
+			label.Text = "0:00";
+			endTimer.Start();
+		}
+	}
+
+	private void endTutorial()
+	{
+		GetTree().ChangeSceneToFile("res://Scenes/Yusuf/startscherm.tscn");
+	}
+}
