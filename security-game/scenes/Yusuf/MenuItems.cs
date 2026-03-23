302b9f4aedcd22215b94bd87862634567f07e267
Bumblebee10 <123257052+Bumblebee10@users.noreply.github.com>
2026-03-23 15:24:34 +0100

    Small additions
    - Added wall behind example door in tutorial level
    - Changed tired note text
    - Title screen button light up when hovering over them

--- security-game/scenes/Yusuf/MenuItems.cs
+++ security-game/scenes/Yusuf/MenuItems.cs
@@ -6,6 +6,11 @@ public partial class MenuItems : Node3D
 {
 	[Export] private AnimationPlayer _animationPlayer;
 
+	[Export] private MeshInstance3D playMesh;
+	[Export] private MeshInstance3D quitMesh;
+
+	private StandardMaterial3D mat;
+
 
 	private void StartButton(Node camera, InputEvent @event, Vector3 position, Vector3 normal, int shapeIdx)
 	{
@@ -20,6 +25,42 @@ public partial class MenuItems : Node3D
 		if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
 		{
 			GetTree().Quit();
+		}
+	}
+
+	private void playHighlight()
+	{
+		mat = playMesh.GetActiveMaterial(0) as StandardMaterial3D;
+		if (mat != null)
+		{
+			mat.EmissionEnabled = true;
+		}
+	}
+
+	private void playUnHighlight()
+	{
+		mat = playMesh.GetActiveMaterial(0) as StandardMaterial3D;
+		if (mat != null)
+		{
+			mat.EmissionEnabled = false;
+		}
+	}
+
+	private void quitHighlight()
+	{
+		mat = quitMesh.GetActiveMaterial(0) as StandardMaterial3D;
+		if (mat != null)
+		{
+			mat.EmissionEnabled = true;
+		}
+	}
+
+	private void quitUnHighlight()
+	{
+		mat = quitMesh.GetActiveMaterial(0) as StandardMaterial3D;
+		if (mat != null)
+		{
+			mat.EmissionEnabled = false;
 		}
 	}
 
