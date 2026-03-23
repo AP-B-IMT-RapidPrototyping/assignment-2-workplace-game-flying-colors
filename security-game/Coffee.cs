0972ede6143418c9b67e085f488c59d7a2ebad3e
Bumblebee10 <123257052+Bumblebee10@users.noreply.github.com>
2026-03-23 14:47:14 +0100

    Coffee drink feedback
    There's now visual feedback when you drink coffee

--- security-game/Coffee.cs
+++ security-game/Coffee.cs
@@ -5,10 +5,24 @@ public partial class Coffee : Node3D, IInteractible
 {
 	[Signal]
 	public delegate void DrinkEventHandler(Node3D interactor, Vector3 hitPosition);
+	[Export] private Label coffeeText;
+	[Export] private Timer visibilityTimer;
+
+    public override void _Ready()
+    {
+        visibilityTimer.Timeout += eraseText;
+    }
 
 	public void Interact(Node3D interactor, Vector3 hitPosition)
 	{
 		GD.Print("Drink signal emitted");
 		EmitSignal(SignalName.Drink, 1f);
+		coffeeText.Visible = true;
+		visibilityTimer.Start();
+	}
+
+	private void eraseText()
+	{
+		coffeeText.Visible = false;
 	}
 }
