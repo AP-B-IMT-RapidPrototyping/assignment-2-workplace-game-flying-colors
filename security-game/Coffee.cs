e0d72ec25e2388a2274bdb62b59097bbfe1e6989
a <marsdding@gmail.com>
2026-03-24 13:16:55 +0100

    Added coffee audio

--- security-game/Coffee.cs
+++ security-game/Coffee.cs
@@ -7,16 +7,22 @@ public partial class Coffee : Node3D, IInteractible
 	public delegate void DrinkEventHandler(Node3D interactor, Vector3 hitPosition);
 	[Export] private Label coffeeText;
 	[Export] private Timer visibilityTimer;
+	[Export] private AudioStreamPlayer3D audioPlayer;
+	[Export] private AudioStream drinkSound;
 
-    public override void _Ready()
-    {
-        visibilityTimer.Timeout += eraseText;
-    }
+	public override void _Ready()
+	{
+		if (audioPlayer == null)
+			audioPlayer = GetNodeOrNull<AudioStreamPlayer3D>("AudioStreamPlayer3D");
+
+		visibilityTimer.Timeout += eraseText;
+	}
 
 	public void Interact(Node3D interactor, Vector3 hitPosition)
 	{
 		GD.Print("Drink signal emitted");
 		EmitSignal(SignalName.Drink, 1f);
+		PlaySound();
 		coffeeText.Visible = true;
 		visibilityTimer.Start();
 	}
@@ -24,5 +30,14 @@ public partial class Coffee : Node3D, IInteractible
 	private void eraseText()
 	{
 		coffeeText.Visible = false;
+	}
+
+	private void PlaySound()
+	{
+		if (audioPlayer == null || drinkSound == null)
+			return;
+
+		audioPlayer.Stream = drinkSound;
+		audioPlayer.Play();
 	}
 }
